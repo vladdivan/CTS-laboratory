7eb9090509ecc98e12a57b8aabc854351423104f
vladdivan <ivanvlad18@stud.ase.ro>
2021-03-16 09:44:28 +0200

    Update Account.java

--- Week2 - JavaBasics/src/ro/ase/csie/cts/g1092/week2/Account.java
+++ Week2 - JavaBasics/src/ro/ase/csie/cts/g1092/week2/Account.java
@@ -1,9 +1,12 @@
 package ro.ase.csie.cts.g1092.week2;
 
+import ro.ase.csie.cts.g1092.week2.exceptions.IllegalTransferException;
+import ro.ase.csie.cts.g1092.week2.exceptions.InsufficientFundsException;
+
 public abstract class Account {
 	public abstract double getBalance();
 	public abstract void deposit(Object amount);
-	public abstract void transfer(Account destination, double amount);
-	public abstract void withdraw(double amount);
+	public abstract void transfer(Account destination, double amount) throws InsufficientFundsException, IllegalTransferException;
+	public abstract void withdraw(double amount) throws InsufficientFundsException;
 	
 }
